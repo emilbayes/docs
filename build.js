d9b1e77f0a7a1153e6ebbe1084560b0d0f16e849
Joe Hand <joe@joeahand.com>
2016-08-05 13:12:20 -0700

    move ecosystem docs module to subdir

--- build.js
+++ build.js
@@ -1,6 +1,14 @@
 var fs = require('fs')
 var ndjson = require('ndjson')
 
+try {
+  fs.statSync('docs/modules/', function (err, stat) {
+    if (!stat.isDirectory()) fs.mkdirSync('docs/modules/')
+  })
+} catch (e) {
+  fs.mkdirSync('docs/modules/')
+}
+
 process.stdin.pipe(ndjson.parse()).on('data', function (obj) {
-  fs.writeFileSync('docs/' + obj.name + '.md', obj.readme)
+  fs.writeFileSync('docs/modules/' + obj.name + '.md', obj.readme)
 })
